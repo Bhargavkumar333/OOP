8a06ac63de1cde237316657bbb9e6763947e9bf7
Bhargavkumar333 <manthribhargavkumar@gmail.com>
2025-08-02 10:08:26 +0530

    Add files via upload

---
+++ Skill_task1_oop/src/mypackage/PrintingSum.java
@@ -0,0 +1,10 @@
+package mypackage;
+
+public class PrintingSum {
+
+		public static void main(String[] args) {
+			int a=10;
+			int b=20;
+			System.out.println("Result of Sum: " +(a+b));
+		}
+	}
